2a189bcc95fcfaf61adace076c9baacf32ad2fb4
Chi-G <chijindu.nwokeohuru@gmail.com>
2023-08-07 05:10:39 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,23 +7,21 @@
  * @height: height input
  * Return: pointer to 2 dim. array
  */
+
 int **alloc_grid(int width, int height)
 {
 int **mee;
 int x, y;
-        
+
 if (width <= 0 || height <= 0)
 return (NULL);
-
 mee = malloc(sizeof(int *) * height);
-
 if (mee == NULL)
 return (NULL);
 
 for (x = 0; x < height; x++)
-{	
+{
 mee[x] = malloc(sizeof(int) * width);
-
 if (mee[x] == NULL)
 {
 for (; x >= 0; x--)
@@ -34,10 +32,9 @@ return (NULL);
 }
 
 for (x = 0; x < height; x++)
-{	
+{
 for (y = 0; y < width; y++)
 mee[x][y] = 0;
 }
 return (mee);
 }
-
