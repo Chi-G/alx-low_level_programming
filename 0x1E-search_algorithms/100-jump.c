ad2e1bce5d9589a5507af6e5570042f9f47cb841
Chijindu Nwokeohuru <chijindu.nwokeohuru@gmail.com>
2024-04-14 22:48:55 +0100

    new commit update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,15 +1,44 @@
 #include "search_algos.h"
+#include <math.h>
+#include <stdio.h>
 
 /**
- *  * jump_search - a function that searches for a value in a sorted array
- *   * of integers using the Jump search algorithm
- *    * @array: a pointer to the first element of the array to search in
- *     * @size: the number of elements in array
- *      * @value: the value to search for
- *       *
- *        * Return: the first index where value is located
- *         */
+ * jump_search - Searches for a value in a sorted array of integers
+ *               using the Jump search algorithm.
+ * @array: A pointer to the first element of the array to search in.
+ * @size: The number of elements in the array.
+ * @value: The value to search for.
+ *
+ * Return: The first index where value is located, or -1 if the value is
+ *         not present or the array is NULL.
+ */
 int jump_search(int *array, size_t size, int value)
 {
+    if (array == NULL || size == 0) {
+        return -1;
+    }
 
+    size_t step = sqrt(size);
+    size_t prev = 0;
+
+    while (array[min(step, size) - 1] < value) {
+        prev = step;
+        step += sqrt(size);
+        if (prev >= size) {
+            return -1;
+        }
+    }
+
+    while (array[prev] < value) {
+        prev++;
+        if (prev == min(step, size)) {
+            return -1;
+        }
+    }
+
+    if (array[prev] == value) {
+        return prev;
+    }
+
+    return -1;
 }
