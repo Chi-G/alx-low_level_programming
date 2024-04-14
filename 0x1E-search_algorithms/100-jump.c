93aa3d7d2b334222ab3f0444de4d834d23746ade
Chijindu Nwokeohuru <chijindu.nwokeohuru@gmail.com>
2024-04-14 22:51:26 +0100

    new commit update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,13 +14,16 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
+    size_t step;
+    size_t prev;
+
     if (array == NULL || size == 0) {
         return -1;
     }
 
-    size_t step = sqrt(size);
-    size_t prev = 0;
-
+    step = sqrt(size);
+    prev = 0;
+    
     while (array[min(step, size) - 1] < value) {
         prev = step;
         step += sqrt(size);
